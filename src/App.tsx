95ff7c469a7eff5dd44dfb8af90b9b6287d6ceeb
MdeMyke <maicolpina8@gmail.com>
2025-07-02 12:46:31 -0500

    feat: Actualización de dependencias en package.json y package-lock.json, incluyendo nuevas bibliotecas para mejorar la funcionalidad de la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/layout/Header';
 import Hero from './components/sections/Hero';
 import Services from './components/sections/Services';
 import About from './components/sections/About';
-import Companies from './components/sections/companies';
+import Companies from './components/sections/Companies';
 import Contact from './components/sections/Contact';
 
 function App() {
